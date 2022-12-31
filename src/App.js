d84318f0ab9afb5aa925238d2c0f9908ccf46458
alwinpamintuan <alwinpamintuan@gmail.com>
2022-12-31 17:04:16 +0800

    Add view source and update main css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import { AppContextProvider } from './context/AppContext';
-import Sidebar from './components/Sidebar/Sidebar';
-import Main from './components/Main/Main';
-import './App.css';
+import { AppContextProvider } from "./context/AppContext";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Main from "./components/Main/Main";
+import "./App.css";
+import logo from "./assets/logowhite.png";
 
 function App() {
   return (
     <AppContextProvider>
-      <div className="App">
-        <Sidebar/>
-        <Main/>
+      <div className="app">
+        <div id="header">
+          <h1>RAFFLER</h1>
+          <img src={logo} className="logo" />
+          <a
+            href="https://github.com/alwinpamintuan/raffler"
+            target="_blank"
+            rel="noreferrer"
+            className="github"
+          >
+            GITHUB
+          </a>
+        </div>
+        <div className="content">
+          <Sidebar />
+          <Main />
+        </div>
       </div>
     </AppContextProvider>
   );
